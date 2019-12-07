2f04dd5c8cad2e6262a75bf42b83029ef3f4fbf6
Moizez <moizezhenrique@gmail.com>
2019-12-07 15:28:26 -0300

    Mudanças no metodo para POST

--- src/main/java/net/milkpoint/api/LoginResource.java
+++ src/main/java/net/milkpoint/api/LoginResource.java
@@ -5,9 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +18,9 @@ public class LoginResource {
 	@Autowired
 	LoginService service;
 	
-	@GetMapping("/login")
+	
+	//@RequestMapping(method = { RequestMethod.POST })
+	@PostMapping("/login")
 	public ResponseEntity<Object> login(String email, String senha) {
 		if(email != null && senha != null) {
 			Object obj = service.login(email, senha);
